dff5ef437a437826337e44b44c05460cab5f337a
ahmed <ahmedelmeqdamy223@gmail.com>
2025-05-19 00:54:55 +0300

    search view Done!!

--- lib/features/search/presentation/views/widgets/search_result_list_view.dart
+++ lib/features/search/presentation/views/widgets/search_result_list_view.dart
@@ -1,14 +1,14 @@
-import 'package:clean_bookly_code/features/home/presentation/views/widgets/best_seller_list_view_item.dart';
 import 'package:flutter/material.dart';
 
-class BestSellerListView extends StatelessWidget {
-  const BestSellerListView({super.key});
+import '../../../../home/presentation/views/widgets/best_seller_list_view_item.dart';
+class SearchResultListView extends StatelessWidget {
+  const SearchResultListView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
+    return  ListView.builder(
       shrinkWrap: true,
-      physics: NeverScrollableScrollPhysics(),
+      // physics: NeverScrollableScrollPhysics(),
       itemCount: 10,
       itemBuilder: (context, index) {
         return Padding(
--- lib/features/search/presentation/views/widgets/search_view_body.dart
+++ lib/features/search/presentation/views/widgets/search_view_body.dart
@@ -1,12 +1,26 @@
+import 'package:clean_bookly_code/core/utliti/styles.dart';
+import 'package:clean_bookly_code/features/search/presentation/views/widgets/search_result_list_view.dart';
 import 'package:clean_bookly_code/features/search/presentation/views/widgets/text_form_field.dart';
 import 'package:flutter/material.dart';
+
 class SearchViewBody extends StatelessWidget {
   const SearchViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  CustomTextFormField(hintText: 'search...',
-
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomTextFormField(hintText: 'search...'),
+          Text(
+            'Results',
+            style: Styles.textStyle21.copyWith(fontFamily: 'Gt Sectra Fine'),
+          ),
+          Expanded(child: SearchResultListView()),
+        ],
+      ),
     );
   }
 }
